79307ded8be5f0e218e94c466197c419d771b91c
stu <s+github@stu.scot>
2022-07-31 11:54:35 +0100

    Add initial library

---
+++ include/sfun/functional.h
@@ -0,0 +1,176 @@
+#ifndef STATIC_FUNCTIONAL_INCLUDE_SFUN_FUNCTIONAL_H
+#define STATIC_FUNCTIONAL_INCLUDE_SFUN_FUNCTIONAL_H
+#include <sfun/type_list.h>
+#include <functional>
+#include <type_traits>
+
+namespace sfun {
+namespace detail {
+struct function_traits_not_impl {
+  inline static constexpr bool is_function = false;
+  inline static constexpr bool is_function_ptr = false;
+  inline static constexpr bool is_function_ref = false;
+  inline static constexpr bool is_member_function_ptr = false;
+};
+
+template <bool Type, bool Ptr, bool Ref, bool MemPtr, typename R, typename... Args>
+struct function_traits_impl {
+  using function_type = R(Args...);
+  using return_type = R;
+  using parameter_types = list<Args...>;
+  inline static constexpr bool is_function_type = Type;
+  inline static constexpr bool is_function_ptr = Ptr;
+  inline static constexpr bool is_function_ref = Ref;
+  inline static constexpr bool is_member_function_ptr = MemPtr;
+};
+}  // namespace detail
+
+template <typename>
+struct function_traits : detail::function_traits_not_impl {};
+template <typename R, typename... Args>
+struct function_traits<R(Args...)>
+: detail::function_traits_impl<true, false, false, false, R, Args...> {};
+template <typename R, typename... Args>
+struct function_traits<R (*)(Args...)>
+: detail::function_traits_impl<false, true, false, false, R, Args...> {};
+template <typename R, typename... Args>
+struct function_traits<R (*const)(Args...)>
+: detail::function_traits_impl<false, true, false, false, R, Args...> {};
+template <typename R, typename... Args>
+struct function_traits<R (&)(Args...)>
+: detail::function_traits_impl<false, false, true, false, R, Args...> {};
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...)>
+: detail::function_traits_impl<false, false, false, true, R, C&, Args...> {};
+template <typename C, typename R, typename... Args>
+struct function_traits<R (C::*)(Args...) const>
+: detail::function_traits_impl<false, false, false, true, R, const C&, Args...> {};
+
+template <typename T>
+concept member_function = function_traits<T>::is_member_function_ptr;
+template <typename T>
+concept function = function_traits<T>::is_function_ptr || function_traits<T>::is_function_ref ||
+    member_function<T>;
+template <typename T>
+concept function_type = function_traits<T>::is_function_type;
+template <typename T>
+concept functional = function<T> || function_type<T>;
+
+template <functional T>
+using function_type_of = typename function_traits<T>::function_type;
+template <functional T>
+using return_type_of = typename function_traits<T>::return_type;
+template <functional T>
+using parameter_types_of = typename function_traits<T>::parameter_types;
+
+template <function_type T>
+using function_ptr = T*;
+
+namespace detail {
+template <member_function auto F, typename>
+struct unwrap_f;
+template <member_function auto F, typename... Args>
+struct unwrap_f<F, list<Args...>> {
+  static inline constexpr decltype(auto) f(Args... args) {
+    return std::invoke(F, args...);
+  }
+};
+template <function auto F, bool MemPtr = member_function<decltype(F)>>
+struct unwrap_if {
+  static inline constexpr auto value = F;
+};
+template <function auto F>
+struct unwrap_if<F, true> {
+  static inline constexpr auto value = &unwrap_f<F, parameter_types_of<decltype(F)>>::f;
+};
+}  // namespace detail
+
+template <function auto F>
+inline constexpr auto unwrap = detail::unwrap_if<F>::value;
+
+namespace detail {
+template <function auto F, typename R, typename, typename, typename, typename>
+struct cast_f;
+template <function auto F, typename R, typename... UsedSourceArgs, typename... UnusedSourceArgs,
+          typename... UsedTargetArgs, typename... UnusedTargetArgs>
+struct cast_f<F, R, list<UsedSourceArgs...>, list<UnusedSourceArgs...>,
+              list<UsedTargetArgs...>, list<UnusedTargetArgs...>> {
+  inline static constexpr decltype(auto) f(UsedTargetArgs... args, UnusedTargetArgs...) {
+    return static_cast<R>(F(static_cast<UsedSourceArgs>(args)..., UnusedSourceArgs{}...));
+  }
+};
+
+template <typename T>
+using is_default_constructible = std::is_constructible<T>;
+template <typename... SourceArgs, typename... TargetArgs>
+constexpr bool all_constructible(list<SourceArgs...>, list<TargetArgs...>) {
+  return (std::is_constructible_v<SourceArgs, TargetArgs> && ...);
+}
+
+template <functional Source, functional Target>
+struct cast_impl {
+  using source_args = parameter_types_of<Source>;
+  using target_args = parameter_types_of<Target>;
+  inline static constexpr auto n =
+      size<source_args> < size<target_args> ? size<source_args> : size<target_args>;
+  using used_source_args = sublist<source_args, 0, n>;
+  using used_target_args = sublist<target_args, 0, n>;
+  using unused_source_args = sublist<source_args, n>;
+  using unused_target_args = sublist<target_args, n>;
+
+  inline static constexpr bool return_type_convertible =
+      std::is_same_v<return_type_of<Target>, void> ||
+      std::is_constructible_v<return_type_of<Target>, return_type_of<Source>>;
+  inline static constexpr bool matching_parameters_convertible =
+      all_constructible(used_source_args{}, used_target_args{});
+  inline static constexpr bool missing_parameters_default_constructible =
+      all_of<unused_source_args, is_default_constructible>;
+
+  template <function auto F>
+  inline static constexpr auto cast =
+      &cast_f<F, return_type_of<Target>, used_source_args, unused_source_args, used_target_args,
+              unused_target_args>::f;
+};
+
+template <function_type T, function auto F,
+          bool IsSame = std::is_same_v<T, function_type_of<decltype(F)>>>
+struct cast_if {
+  static inline constexpr auto value = F;
+};
+template <function_type T, function auto F>
+struct cast_if<T, F, false> {
+  static inline constexpr auto value = cast_impl<decltype(F), T>::template cast<F>;
+};
+
+template <typename, function auto... F>
+struct sequence_f;
+template <typename... Args, function auto... F>
+struct sequence_f<list<Args...>, F...> {
+  inline static constexpr decltype(auto) f(Args... args) {
+    return (F(args...), ...);
+  }
+};
+}  // namespace detail
+
+template <typename T, typename... Rest>
+concept same_parameters = functional<T> &&(functional<Rest>&&...) &&
+    (std::is_same_v<parameter_types_of<T>, parameter_types_of<Rest>> && ...);
+
+template <typename Source, typename Target>
+concept castable_to = functional<Source> && functional<Target> &&
+    detail::cast_impl<Source, Target>::return_type_convertible &&
+    detail::cast_impl<Source, Target>::matching_parameters_convertible &&
+    detail::cast_impl<Source, Target>::missing_parameters_default_constructible;
+
+template <function auto F, function auto... Rest>
+requires same_parameters<decltype(F), decltype(Rest)...>
+inline constexpr auto sequence =
+    &detail::sequence_f<parameter_types_of<decltype(F)>, unwrap<F>, unwrap<Rest>...>::f;
+
+template <function_type T, function auto F>
+requires castable_to<decltype(F), T>
+inline constexpr auto cast = detail::cast_if<T, unwrap<F>>::value;
+
+}  // namespace sfun
+
+#endif
---
+++ include/sfun/type_list.h
@@ -0,0 +1,178 @@
+#ifndef STATIC_FUNCTIONAL_INCLUDE_SFUN_TYPE_LIST_H
+#define STATIC_FUNCTIONAL_INCLUDE_SFUN_TYPE_LIST_H
+#include <cstddef>
+#include <limits>
+#include <type_traits>
+
+namespace sfun {
+
+template <typename...>
+struct list {};
+
+template <typename>
+inline constexpr bool is_type_list = false;
+template <typename... Ts>
+inline constexpr bool is_type_list<list<Ts...>> = true;
+template <typename T>
+concept type_list = is_type_list<T>;
+
+template <type_list>
+inline constexpr std::size_t size = 0;
+template <typename... Ts>
+inline constexpr std::size_t size<list<Ts...>> = sizeof...(Ts);
+template <type_list A>
+inline constexpr bool empty = !size<A>;
+inline constexpr std::size_t end = std::numeric_limits<std::size_t>::max();
+template <template <typename> typename F, typename T>
+using apply = typename F<T>::type;
+
+namespace detail {
+template <typename... As, typename... Bs>
+consteval auto concat_impl(list<As...>, list<Bs...>) -> list<As..., Bs...>;
+template <typename T, typename... Ts>
+consteval auto append_impl(list<Ts...>) -> list<Ts..., T>;
+template <typename T, typename... Ts>
+consteval auto prepend_impl(list<Ts...>) -> list<T, Ts...>;
+template <typename T, typename... Ts>
+consteval auto drop_front_impl(list<T, Ts...>) -> list<Ts...>;
+template <typename T, typename... Ts>
+consteval auto drop_back_impl(list<Ts..., T>) -> list<Ts...>;
+template <typename T, typename... Ts>
+consteval auto front_impl(list<T, Ts...>) -> T;
+template <typename T, typename... Ts>
+consteval auto back_impl(list<Ts..., T>) -> T;
+template <template <typename...> typename Template, typename... Ts>
+consteval auto to_impl(list<Ts...>) -> Template<Ts...>;
+}  // namespace detail
+
+template <type_list A, type_list B>
+using concat = decltype(detail::concat_impl(A{}, B{}));
+template <typename T, type_list A>
+using append = decltype(detail::append_impl<T>(A{}));
+template <typename T, type_list A>
+using prepend = decltype(detail::prepend_impl<T>(A{}));
+template <type_list A>
+requires(!empty<A>) using drop_front = decltype(detail::drop_front_impl(A{}));
+template <type_list A>
+requires(!empty<A>) using drop_back = decltype(detail::drop_back_impl(A{}));
+template <type_list A>
+requires(!empty<A>) using front = decltype(detail::front_impl(A{}));
+template <type_list A>
+requires(!empty<A>) using back = decltype(detail::back_impl(A{}));
+template <type_list A, template <typename...> typename Template>
+using to = decltype(detail::to_impl<Template>(A{}));
+
+namespace detail {
+template <std::size_t N, type_list A>
+consteval auto get_impl(A) {
+  if constexpr (N == 0) {
+    return front<A>{};
+  } else {
+    return get_impl<N - 1>(drop_front<A>{});
+  }
+}
+
+template <std::size_t Index, std::size_t Size, type_list A>
+consteval auto sublist_impl(A) {
+  if constexpr (empty<A> || Size == 0) {
+    return list<>{};
+  } else if constexpr (Index == 0) {
+    return prepend<front<A>, decltype(sublist_impl<0, Size - 1>(drop_front<A>{}))>{};
+  } else {
+    return sublist_impl<Index - 1, Size>(drop_front<A>{});
+  }
+}
+}  // namespace detail
+
+template <type_list A, std::size_t N>
+requires(N < size<A>) using get = decltype(detail::get_impl<N>(A{}));
+template <type_list A, std::size_t Index, std::size_t Size = end>
+requires(Index <= size<A>) using sublist = decltype(detail::sublist_impl<Index, Size>(A{}));
+template <type_list A, std::size_t... N>
+requires((N < size<A>)&&...) using select = list<get<A, N>...>;
+
+namespace detail {
+template <template <typename> typename P, typename... Ts>
+consteval bool all_of_impl(list<Ts...>) {
+  return (P<Ts>::value && ...);
+}
+
+template <template <typename> typename P, typename... Ts>
+consteval bool any_of_impl(list<Ts...>) {
+  return (P<Ts>::value || ...);
+}
+
+template <template <typename> typename P, type_list A>
+consteval std::size_t find_if_impl(A) {
+  if constexpr (empty<A>) {
+    return 0;
+  } else if constexpr (P<front<A>>::value) {
+    return 0;
+  } else {
+    return 1 + find_if_impl<P>(drop_front<A>{});
+  }
+}
+
+template <template <typename> typename P, typename... Ts>
+consteval std::size_t count_if_impl(list<Ts...>) {
+  return ((P<Ts>::value ? 1u : 0u) + ...);
+}
+
+template <template <typename> typename P, type_list A>
+consteval auto filter_impl(A) {
+  if constexpr (empty<A>) {
+    return list<>{};
+  } else if constexpr (P<front<A>>::value) {
+    return prepend<front<A>, decltype(filter_impl<P>(drop_front<A>{}))>{};
+  } else {
+    return filter_impl<P>(drop_front<A>{});
+  }
+}
+
+template <template <typename> typename F, type_list A>
+consteval auto map_impl(A) {
+  if constexpr (empty<A>) {
+    return list<>{};
+  } else {
+    return prepend<apply<F, front<A>>, decltype(map_impl<F>(drop_front<A>{}))>{};
+  }
+}
+
+template <typename T>
+struct same_as_metafunction {
+  template <typename U>
+  struct predicate : std::is_same<T, U> {};
+};
+
+template <template <typename> typename T>
+struct wrap_metafunction {
+  template <typename U>
+  struct functor : std::type_identity<T<U>> {};
+};
+}  // namespace detail
+
+template <type_list A, template <typename> typename P>
+inline constexpr bool all_of = detail::all_of_impl<P>(A{});
+template <type_list A, template <typename> typename P>
+inline constexpr bool any_of = detail::any_of_impl<P>(A{});
+template <type_list A, template <typename> typename P>
+inline constexpr bool none_of = !any_of<A, P>;
+template <type_list A, template <typename> typename P>
+inline constexpr std::size_t find_if = detail::find_if_impl<P>(A{});
+template <type_list A, template <typename> typename P>
+inline constexpr std::size_t count_if = detail::count_if_impl<P>(A{});
+template <type_list A, template <typename> typename P>
+using filter = decltype(detail::filter_impl<P>(A{}));
+template <type_list A, template <typename> typename F>
+using map = decltype(detail::map_impl<F>(A{}));
+
+template <type_list A, typename T>
+inline constexpr std::size_t find = find_if<A, detail::same_as_metafunction<T>::template predicate>;
+template <type_list A, typename T>
+inline constexpr std::size_t count = count_if<A, detail::same_as_metafunction<T>::template predicate>;
+template <type_list A, template <typename> typename T>
+using wrap = map<A, detail::wrap_metafunction<T>::template functor>;
+
+}  // namespace sfun
+
+#endif
